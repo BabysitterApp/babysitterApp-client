55f7e5008a59b5082f0ce452f40623815b0adf1a
Nishtha Agarwal <nishtha.agarwal26@gmail.com>
2023-06-30 15:27:16 +0200

    pages and components initiated

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";                     // <== IMPORT 
+//import { AuthContext } from "../context/auth.context"; 
+
+function Navbar() {
+    const { isLoggedIn, user, logOutUser } = useContext();  
+
+    return (
+        <nav>
+          <Link to="/">
+            <button>Home</button>
+          </Link>
+    
+        
+    
+          {isLoggedIn && (
+            <>
+              <Link to="/babysitterServices">
+                <button>Babysitters </button>
+              </Link>
+            
+            
+          <button onClick={logOutUser}>Logout</button>
+          <span>{user && user.name}</span>
+        </>
+      )}
+
+          {!isLoggedIn && (
+            <>
+              <Link to="/signup">
+                {" "}
+                <button>Sign Up</button>{" "}
+              </Link>
+              <Link to="/login">
+                {" "}
+                <button>Login</button>{" "}
+              </Link>
+            </>
+          )}
+        </nav>
+      );
+    }
+
+export default Navbar;
