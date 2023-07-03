52dfab218092acb2842cbfb98bb920af69dcb884
Mary Sue Meissner <marysue.meissner@outlook.com>
2023-07-03 15:49:54 +0200

    Planning for Styling,  Bootstrap and CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,44 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";                     // <== IMPORT 
-import { AuthContext } from "../context/auth.context"; 
+import { useContext } from "react"; // <== IMPORT
+import { AuthContext } from "../context/auth.context";
 
 function Navbar() {
-    const { isLoggedIn, user, logOutUser } = useContext(AuthContext);  
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
-    return (
-        <nav>
-          <Link to="/">
-            <button>Home</button>
-          </Link>
-    
-        
-    
-          {isLoggedIn && (
-            <>
-              <Link to="/babysitterServices">
-                <button>Babysitters </button>
-              </Link>
-            
-            
-          <button onClick={logOutUser}>Logout</button>
-          <span>{user && user.name}</span>
-        </>
-          )}
+  return (
+    <nav class="navbar bg-body-tertiary">
+      <div class="container">
+        <a class="navbar-brand">LittleLuvSitters</a>
+        <Link to="/">
+          <button>Home</button>
+        </Link>
 
-          {!isLoggedIn && (
-            <>
-              <Link to="/signup">
-                {" "}
-                <button>Sign Up</button>{" "}
-              </Link>
-              <Link to="/login">
-                {" "}
-                <button>Login</button>{" "}
-              </Link>
-            </>
-          )}
-        </nav>
-      );
-    }
+        {isLoggedIn && (
+          <>
+            <Link to="/babysitterServices">
+              <button>Babysitters </button>
+            </Link>
+
+            <button onClick={logOutUser}>Logout</button>
+            <span>{user && user.name}</span>
+          </>
+        )}
+
+        {!isLoggedIn && (
+          <>
+            <Link to="/signup">
+              {" "}
+              <button>Sign Up</button>{" "}
+            </Link>
+            <Link to="/login">
+              {" "}
+              <button>Login</button>{" "}
+            </Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
