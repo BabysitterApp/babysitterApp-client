ff604bb80907d425c877ed37fbef7bd3707da5ab
Nishtha Agarwal <nishtha.agarwal26@gmail.com>
2023-07-04 18:45:02 +0200

    setup api

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -25,7 +25,7 @@ function LoginPage(props) {
     e.preventDefault();
     const requestBody = { email, password };
  
-    axios.post(`${API_URL}/auth/login`, requestBody)
+    axios.post(`${process.env.REACT_APP_SERVER_URL}/auth/login`, requestBody)
     authService.login(requestBody) 
       .then((response) => {
      
